e9b5296c49ab78019aafbd54067feac28ddb9653
Julithub161 <jufu1122@gmail.com>
2023-05-23 13:05:32 +0200

    second commit

--- functions/methods.py
+++ functions/methods.py
@@ -1,3 +1,35 @@
-def addieren(zahl1, zahl2):
-    ergebnis = zahl2 + zahl1
-    return ergebnis
+import datetime
+
+import discord
+from discord.ext import commands
+from discord.utils import get
+intents = discord.Intents.default()
+intents.message_content = True
+
+# Erstelle einen Bot-Client
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Event: Bot ist bereit
+@bot.event
+async def on_ready():
+    print('Bot ist bereit.')
+
+# Command: Ping
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+@bot.command()
+async def online(ctx, username:str):
+    guild = ctx.guild
+    member = get(guild.members, name=username)
+    if member is not None:
+        await ctx.send(f"Der Nutzer {member.name} wurde gefunden.")
+    else:
+        await ctx.send("Der angegebene Nutzer wurde nicht gefunden.")
+
+    await ctx.send("Der User " + username + " ist seit ... online")
+
+
+# Führe den Bot aus
+bot.run('MTA3MjEwMjc1MTY0OTE0NDg1Mg.GrVUZX.A5r-4OadxmfELAzFWqYrFy7gve6_v9qYa3XP3M')
--- main.py
+++
@@ -1,5 +0,0 @@
-# This is a sample Python script.
-import functions.methods as m
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
-print(m.addieren(12, 5))
